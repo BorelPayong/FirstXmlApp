29c1d66db7458d623da2bd0b14e96b4c855cb46e
BorelPayong <68334292+BorelPayong@users.noreply.github.com>
2021-02-01 21:04:26 +0100

    creation classe chatlist

---
+++ FirstXmlApp/Models/ChatList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstXmlApp.Models
+{
+    public class ChatList
+    {
+        public string FullName { get; set; }
+
+        public string Time { get; set; }
+
+        public string Photo { get; set; }
+
+        public string ShortMessage { get; set; }
+
+    }
+}
